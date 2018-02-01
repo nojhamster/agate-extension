028f62bf1cb780603690d92f8284c1e7fb495dda
Yannick Schurter <yannick.schurter@gmail.com>
2018-02-01 09:20:27 +0100

    overall refactoring

--- src/content_script.js
+++ src/content_script.js
@@ -3,157 +3,205 @@
 
   if (!document.querySelector('#fiche_pointage .jour_courant')) { return }
 
-  const recapBody = document.querySelector('#recap_mensuel > tbody')
-  if (!recapBody) { return }
+  const recap = document.querySelector('#recap_mensuel')
+  if (!recap) { return }
 
-  const monthlyDiff = stringToDuration(getTextOf('#fiche_pointage tfoot .diff'))
-  const dailyTotal  = stringToDuration(getTextOf('#fiche_pointage .jour_courant .champs_temps_theorique'))
-  const dailyBreak  = stringToDuration(getTextOf('#fiche_pointage .jour_courant .champs_pause'))
-  const regulation  = stringToDuration(getTextOf('#fiche_pointage .jour_courant span.regulation'))
-  const transfered  = stringToDuration(getTransferedTime())
-  const clockings   = getClockings()
+  let monthlyDiff  = stringToMinutes(getTextOf('#fiche_pointage tfoot .diff'))
+  let regulation   = stringToMinutes(getTextOf('#fiche_pointage .jour_courant span.regulation'))
+  const dailyTotal = stringToMinutes(getTextOf('#fiche_pointage .jour_courant .champs_temps_theorique'))
+  const dailyBreak = stringToMinutes(getTextOf('#fiche_pointage .jour_courant .champs_pause'))
+  const transfered = stringToMinutes(getTransferedTime())
+  const clockings  = getClockings()
 
-  const now       = moment()
-  const worktime  = moment.duration(0)
-  const remaining = moment.duration(dailyTotal)
+  monthlyDiff += transfered
 
-  if (regulation.isValid()) {
-    worktime.add(regulation)
+  const msToMin      = 60000
+  const minBreakTime = 45
+  const breakTaken   = (dailyBreak > 0)
+  const halfDay      = (regulation > 225)
+
+  if (!halfDay && dailyBreak < minBreakTime) {
+    // If the break time lasted less than 45min, remove every minute under 45 from the regulation
+    // If it's a half day (regulation > 3h45), we don't need to regulate the break time
+    regulation += (dailyBreak - minBreakTime)
   }
 
-  if (clockings.length > 0) {
-    clockings.forEach(({ clockIn, clockOut }) => {
-      if (clockIn && clockOut) {
-        // For each clock-in/clock-out couple, add the period to the work time
-        worktime.add(clockOut).subtract(clockIn)
-      } else if (clockIn) {
-        // If the clock-in has no associated clock-out, add the period going from clock-in to current time
-        worktime.add(now.hours(), 'h').add(now.minutes(), 'm').subtract(clockIn)
+  const newBody = document.createElement('tbody')
+
+  newBody.innerHTML = `
+    <tr class="head">
+      <td colspan="2">En bref</td>
+    </tr>
+    <tr>
+      <td class="libelle">Temps de travail</td>
+      <td id="as-worktime"></td>
+    </tr>
+    <tr>
+      <td class="libelle">Temps de pause</td>
+      <td id="as-breaktime"></td>
+    </tr>
+    <tr>
+      <td class="libelle">Régulations</td>
+      <td id="as-regulation"></td>
+    </tr>
+    <tr style="border-top: 1px solid grey">
+      <td class="libelle">Reste à faire</td>
+      <td id="as-remaining"></td>
+    </tr>
+    <tr>
+      <td class="libelle">Fin de la journée</td>
+      <td id="as-endofday"></td>
+    </tr>
+    <tr style="border-top: 1px solid grey">
+      <td class="libelle">Etat horaire J-1</td>
+      <td id="as-mdiff"></td>
+    </tr>
+    <tr>
+      <td class="libelle">Etat horaire actuel</td>
+      <td id="as-cdiff"></td>
+    </tr>
+  `
+
+  recap.appendChild(newBody)
+  updateMetrics()
+
+  /**
+   * Calculate all dynamic values and update DOM
+   */
+  function updateMetrics () {
+    const worktime    = getWorktime()
+    const remaining   = dailyTotal - worktime - regulation
+    const currentDiff = monthlyDiff - remaining
+    const endOfDay    = new Date(Date.now() + remaining * msToMin)
+
+    const borderTop = { 'border-top': '1px solid grey' }
+
+    document.getElementById('as-worktime').textContent   = minutesToString(worktime)
+    document.getElementById('as-breaktime').textContent  = minutesToString(dailyBreak)
+    document.getElementById('as-regulation').textContent = minutesToString(regulation)
+    document.getElementById('as-remaining').textContent  = minutesToString(remaining)
+    document.getElementById('as-endofday').textContent   = dateToString(endOfDay)
+    document.getElementById('as-mdiff').textContent      = minutesToString(monthlyDiff)
+    document.getElementById('as-cdiff').textContent      = minutesToString(currentDiff)
+
+    // Update metrics every 15s
+    setInterval(updateMetrics, 15000)
+  }
+
+  /**
+   * Get the total worktime based on clockings
+   */
+  function getWorktime () {
+    let worktime = 0
+
+    if (clockings.length > 0) {
+      clockings.forEach(({ clockIn, clockOut }) => {
+        if (clockIn && clockOut) {
+          // For each clock-in/clock-out couple, add the period to the work time
+          worktime += (clockOut - clockIn)
+        } else if (clockIn) {
+          // If the clock-in has no associated clock-out, add the period going from clock-in to current time
+          const now = new Date()
+          worktime += (now.getHours() * 60) + now.getMinutes() - clockIn
+        }
+      })
+    }
+
+    return worktime
+  }
+
+  /**
+   * Get the clockings of the current day
+   */
+  function getClockings () {
+    const clockings = document.querySelectorAll('#fiche_pointage .jour_courant .couple_pointage')
+
+    return Array.from(clockings || []).map(clocking => {
+      const clockIn = parseFloat(clocking.getAttribute('data-entree'))
+      const clockOut = parseFloat(clocking.getAttribute('data-sortie'))
+
+      return {
+        clockIn: isNaN(clockIn) ? null : clockIn,
+        clockOut: isNaN(clockOut) ? null : clockOut
       }
     })
   }
 
-  if (dailyBreak.asMinutes() > 0 && dailyBreak.asMinutes() < 45) {
-    // If the break time has been taken and lasted less than 45min,
-    // subtract every minute under 45 from the work time
-    worktime.add(dailyBreak).subtract(45, 'm')
+  /**
+   * Get work time transfered from the previous month
+   */
+  function getTransferedTime () {
+    const recap = document.querySelectorAll('#recap_mensuel td')
+
+    for (let i = 0; i < recap.length; i++) {
+      if (/report/i.test(recap[i].textContent)) {
+        const nextCell = recap[i + 1]
+        return nextCell && nextCell.textContent
+      }
+    }
   }
 
-  remaining.subtract(worktime)
-  monthlyDiff.add(transfered)
-
-  const endOfDay  = moment(now).add(remaining)
-
-  if (!dailyBreak.asMinutes()) {
-    // If the break time has not been taken yet, assume it will last 45min
-    endOfDay.add(45, 'm')
+  /**
+   * Return the text content of a selector
+   * @param {String} selector
+   */
+  function getTextOf (selector) {
+    const element = document.querySelector(selector)
+    const content = (element && element.textContent) || ''
+    return content.trim()
   }
 
-  recapBody.appendChild(createHeaderRow(), recapBody.firstChild)
-  recapBody.appendChild(createRow('Temps de travail effectif', durationToString(worktime)), recapBody.firstChild)
-  recapBody.appendChild(createRow('Reste à faire', durationToString(remaining)), recapBody.firstChild)
-  recapBody.appendChild(createRow('Fin de la journée', endOfDay.format('HH[h]mm')), recapBody.firstChild)
-  recapBody.appendChild(createRow('Avance J-1', durationToString(monthlyDiff)), recapBody.firstChild)
+  /**
+   * Return the number of minutes from midnight
+   */
+  function minutesFromMidnight () {
+    const now = new Date()
+    return (now.getHours() * 60) + now.getMinutes()
+  }
+
+  /**
+   * Convert a string (..h.. or -..h..) into a number of minutes
+   * @param {String} str
+   */
+  function stringToMinutes (str) {
+    str = (str || '').trim()
+    const match = /^(-)?(\d+)h(\d+)$/.exec(str)
+    if (!match) { return 0 }
+
+    const minutes = (parseInt(match[2]) * 60) + parseInt(match[3])
+
+    return match[1] ? minutes * -1 : minutes
+  }
+
+  /**
+   * Take a number of minutes and return a formated string (..h.. or -..h..)
+   * @param {Integer} nbMinutes
+   */
+  function minutesToString (nbMinutes) {
+    if (!nbMinutes && nbMinutes !== 0) { return 'N/A' }
+
+    let hours = Math.floor(Math.abs(nbMinutes / 60))
+    let minutes = Math.abs(nbMinutes % 60)
+
+    if (hours < 10) { hours = `0${hours}` }
+    if (minutes < 10) { minutes = `0${minutes}` }
+
+    return nbMinutes >= 0 ? `${hours}h${minutes}` : `-${hours}h${minutes}`
+  }
+
+  /**
+   * Take a date and return a formated string (..h.. or -..h..)
+   * @param {Integer} date
+   */
+  function dateToString (date) {
+    if (!date) { return 'N/A' }
+
+    let hours = date.getHours()
+    let minutes = date.getMinutes()
+
+    if (hours < 10) { hours = `0${hours}` }
+    if (minutes < 10) { minutes = `0${minutes}` }
+
+    return `${hours}h${minutes}`
+  }
 })()
-
-/**
- * Create the header of the new section
- */
-function createHeaderRow () {
-  const headerRow = document.createElement('tr')
-  headerRow.classList.add('head')
-
-  const title = document.createElement('td')
-  title.colSpan = 2
-  title.textContent = 'En bref'
-
-  headerRow.appendChild(title)
-
-  return headerRow
-}
-
-/**
- * Add a label/value row to the new section
- * @param {String} label
- * @param {String} value
- */
-function createRow(label, value) {
-  const row = document.createElement('tr')
-
-  const labelCell = document.createElement('td')
-  labelCell.classList.add('libelle')
-  labelCell.textContent = label
-
-  const valueCell = document.createElement('td')
-  valueCell.textContent = value
-
-  row.appendChild(labelCell)
-  row.appendChild(valueCell)
-
-  return row
-}
-
-/**
- * Get the clockings of the current day
- */
-function getClockings () {
-  const clockings = document.querySelectorAll('#fiche_pointage .jour_courant .couple_pointage')
-
-  return Array.from(clockings || []).map(clocking => {
-    const clockIn = moment.duration(parseFloat(clocking.getAttribute('data-entree')), 'm')
-    const clockOut = moment.duration(parseFloat(clocking.getAttribute('data-sortie')), 'm')
-
-    return {
-      clockIn: clockIn.isValid() ? clockIn : null,
-      clockOut: clockOut.isValid() ? clockOut : null
-    }
-  })
-}
-
-/**
- * Get work time transfered from the previous month
- */
-function getTransferedTime () {
-  const recap = document.querySelectorAll('#recap_mensuel td')
-
-  for (let i = 0; i < recap.length; i++) {
-    if (/report/i.test(recap[i].textContent)) {
-      const nextCell = recap[i + 1]
-      return nextCell && nextCell.textContent
-    }
-  }
-}
-
-/**
- * Return the text content of a selector
- * @param {String} selector
- */
-function getTextOf(selector) {
-  const element = document.querySelector(selector)
-  const content = (element && element.textContent) || ''
-  return content.trim()
-}
-
-/**
- * Convert a string (..h.. or -..h..) into a momentjs duration
- * @param {String} str
- */
-function stringToDuration (str) {
-  return moment.duration((str || '').replace('h', ':'))
-}
-
-/**
- * Take a momentjs duration and return a formated string (..h.. or -..h..)
- * @param {Moment.duration} duration
- */
-function durationToString (duration) {
-  if (!duration || !duration.isValid()) { return 'N/A'; }
-
-  let hours = Math.abs(duration.hours())
-  let minutes = Math.abs(duration.minutes())
-
-  if (hours < 10) { hours = `0${hours}` }
-  if (minutes < 10) { minutes = `0${minutes}` }
-
-  return duration.asMinutes() >= 0 ? `${hours}h${minutes}` : `-${hours}h${minutes}`
-}
