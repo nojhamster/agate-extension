80227e003710cdaf1dfec3e078ec102a737e007f
Yannick Schurter <yannick.schurter@gmail.com>
2021-03-19 19:22:11 +0100

    fix: load autocomplete script in janus.cnrs.fr

--- js/preload.js
+++ js/preload.js
@@ -2,7 +2,7 @@ if (window.location.host.endsWith('agateb.cnrs.fr')) {
   require('../extension/content_script.js')
 }
 
-if (window.location.host === 'cas.cnrs.fr') {
+if (window.location.host === 'janus.cnrs.fr') {
   window.addEventListener('load', () => {
     const username = document.getElementById('username')
     username.value = localStorage.getItem('username') || ''
