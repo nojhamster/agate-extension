338f3c2c8ecc5bd4f613ccf34862b3db05b5a8cc
Yannick Schurter <yannick.schurter@gmail.com>
2024-10-25 17:23:37 +0200

    build(extension): set author as string for manifest v2

--- extension/wxt.config.ts
+++ extension/wxt.config.ts
@@ -3,9 +3,10 @@ import { defineConfig } from 'wxt';
 // See https://wxt.dev/api/config.html
 export default defineConfig({
   modules: ['@wxt-dev/module-vue'],
-  manifest: {
-    author: {
-      email: 'yannick.schurter@gmail.com'
-    }
+  manifest: ({ manifestVersion }) => {
+    const email = 'yannick.schurter@gmail.com';
+    return {
+      author: manifestVersion === 2 ? email : { email }
+    };
   }
 });
